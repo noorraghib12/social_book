2143ed4dc3e138f0dd015f23fcb8f5a9ebb43c95
noor.raghib.12 <noor.raghib.12@gmail.com>
2023-10-12 18:13:15 +0600

    12oct2023

--- core/models.py
+++ core/models.py
@@ -6,9 +6,9 @@ User = get_user_model()
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.ForeignKey(User,on_delete=models.cascade)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     id_user = models.IntegerField()
-    bio = models.TextField(blank=True)
+    bio = models.TextField(blank=True) 
     location = models.CharField(max_length=200,blank=True)
     profileimg = models.ImageField(upload_to='profile_images',default='blank-profile-picture.png')
 
--- social_book/urls.py
+++ social_book/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
 
 ]
 
-urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns + static(settings.MEDIA_URL,
+                                   document_root=settings.MEDIA_ROOT)
